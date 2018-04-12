8cc3f74dc590e2d8eacea7006b720391d7b4c87a
Vicky Jáuregui <jaureguivictoria@gmail.com>
2018-04-12 11:48:27 -0300

    Changing name clause

--- resources/views/payments/debtors.blade.php
+++ resources/views/payments/debtors.blade.php
@@ -22,9 +22,9 @@
                     <tr>
                         <td>
                             @if (!empty($showTotal))
-                                <a href="{{route('members.show',$debtor->id)}}">{{$debtor->name}} {{$debtor->surname}}</a>
+                                <a href="{{route('members.show',$debtor->id)}}">{{$debtor->gname}} {{$debtor->surname}}</a>
                             @else
-                                {{$debtor->name}} {{$debtor->surname}}
+                                {{$debtor->gname}} {{$debtor->surname}}
                             @endif
                         </td>
                         @if (!empty($showTotal))
